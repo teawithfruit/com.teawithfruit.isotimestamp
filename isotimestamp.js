b205559ac287502740b8a7ba65ca8150b26a70a0
teawithfruit <teawithfruit@gmail.com>
2021-12-20 13:32:22 +0100

    Create isotimestamp.js

---
+++ isotimestamp.js
@@ -0,0 +1,14 @@
+var ISOTimestampDynamicValue = function() {
+  this.evaluate = function() {
+    return (new Date()).toISOString();
+  };
+  this.title = function() {
+    return "ISO timestamp";
+  };
+};
+
+ISOTimestampDynamicValue.identifier = "com.teawithfruit.timestampiso";
+ISOTimestampDynamicValue.title = "ISO timestamp";
+ISOTimestampDynamicValue.inputs = [];
+
+registerDynamicValueClass(ISOTimestampDynamicValue);
